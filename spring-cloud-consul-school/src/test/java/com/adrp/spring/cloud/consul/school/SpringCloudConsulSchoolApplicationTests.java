c3c31041ed1f8996c7275a028446c59a1091d2e2
Adriana Pricina <andra_adriana28@yahoo.com>
2022-04-15 12:35:42 +0300

    Added rest based student service which talks with Consul Server/Agent to register itself in the service registry Added school service which invokes Student service with service look up mechanism

---
+++ spring-cloud-consul-school/src/test/java/com/adrp/spring/cloud/consul/school/SpringCloudConsulSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrp.spring.cloud.consul.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConsulSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
